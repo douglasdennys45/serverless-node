19cb6f62bd5ecd5a5687e725cbf97b28a3a21a2c
Douglas Dennys <douglasdennys45@gmail.com>
2021-11-05 20:44:55 -0300

    correçao em serverless

--- src/frameworks/database/dynamodb/dynamodb-connect.ts
+++ src/frameworks/database/dynamodb/dynamodb-connect.ts
@@ -4,7 +4,16 @@ export const DynamoDBConnect = {
   client: null as any,
 
   connect (): void {
-    this.client = new AWS.DynamoDB.DocumentClient()
+    const isTest = process.env.JEST_WORKER_ID
+    const config = {
+      convertEmptyValues: true,
+      ...(isTest && {
+        endpoint: 'localhost:3000',
+        sslEnabled: false,
+        region: 'local-env'
+      })
+    }
+    this.client = new AWS.DynamoDB.DocumentClient(config)
   },
 
   run (): any {
